0ca64e199d216da78869b52a1a03258a6273367b
vignesh <vigneshkanth@gmail.com>
2013-04-10 01:07:26 -0400

    added favicon, css framework, member details

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'rails', '3.2.13'
 gem 'sqlite3'
 gem 'jquery-rails'
+gem 'neat'
 
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,9 @@
 class StaticController < ApplicationController
-  def index
-  end
+  require 'open-uri'
+	require 'json'
+ 
+ 	def index
+	end
 
   def events
   end
@@ -9,5 +12,30 @@ class StaticController < ApplicationController
   end
 
   def members
+  	@data = sorted
   end
 end
+
+private
+
+# lopps through each league, sorts all players belonging to that league by their points, adds players to the members array
+# return value is an array of players by their ladder ranking
+def sorted
+	members = []
+	temp = []
+
+	object = JSON.parse(open("http://sc2ranks.com/api/clist/11658/us/all/1/0.json?appKey=uiucstarcraft.com").read)
+  ["grandmaster", "master", "diamond", "platinum", "gold", "silver", "bronze"].each do |league|
+  	object.each do |player|
+  		if player["league"] == "#{league}"
+  			temp.push([player["members"][0]["name"], player["members"][0]["fav_race"], player["league"], player["points"]])
+  		end
+  	end
+  	temp.sort! { |a, b| b[3]<=>a[3] }
+		members += temp
+		temp.clear
+  end
+
+  return members
+end
+
