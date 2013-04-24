bde62cc8d442f5da0a87511b976bab238d0bcfe2
vignesh <vigneshkanth@gmail.com>
2013-04-24 05:41:04 -0400

    mailer in devlopment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,4 +65,14 @@ Csl::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'example.com',
+    :user_name            => 'uiucstarcraft@gmail.com',
+    :password             => 'marineimba',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
+    
 end
